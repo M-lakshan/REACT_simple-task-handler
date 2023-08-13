8bdae4ff335eebdb21f29afd4487c70ff1899828
M-lakshan <emlakshan92@gmail.com>
2023-08-13 16:00:31 +0530

    task list created as for default data

---
+++ src/components/task_list.js
@@ -0,0 +1,56 @@
+export const tasks = [
+  {
+    id: 1,
+    name: "do grocery",
+    details: "by dairy products & eggs",
+    scheduled_for: ["08/18",""],
+    completed: false,
+    removed: false,
+    tags: []
+  },
+  {
+    id: 2,
+    name: "take the trash out",
+    details: "",
+    scheduled_for: ["",""],
+    completed: false,
+    removed: false,
+    tags: []
+  },
+  {
+    id: 3,
+    name: "clean up the fridge",
+    details: "",
+    scheduled_for: ["",""],
+    completed: false,
+    removed: false,
+    tags: []
+  },
+  {
+    id: 4,
+    name: "practice more in programming",
+    details: "follow up REACT",
+    scheduled_for: ["08/20",""],
+    completed: false,
+    removed: false,
+    tags: []
+  },
+  {
+    id: 5,
+    name: "attend the Buisiness meeting",
+    details: "@8.30pm on regional-offce",
+    scheduled_for: ["08/20","07:00pm"],
+    completed: false,
+    removed: true,
+    tags: []
+  },
+  {
+    id: 6,
+    name: "be home by 11.00pm",
+    details: "...hv to sleep ealry in order to attend head-office tommorrow",
+    scheduled_for: ["08/20","10:00pm"],
+    completed: false,
+    removed: false,
+    tags: []
+  },
+];
