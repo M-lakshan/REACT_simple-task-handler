6975243d4509b62687a60c8b4115607f1dceb924
M-lakshan <emlakshan92@gmail.com>
2023-08-15 17:14:32 +0530

    minor fixes, yet to implement the 'seconds run'

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
 import logo from './../imgs/logo.png';
 import getDateTime from './date_n_time.js';
+import {useEffect} from 'react';
 
 const Header = ({sub_title,date_n_time}) => {
+  let date = new Date();
 
   return (
     <header className="App-header">
@@ -13,14 +15,22 @@ const Header = ({sub_title,date_n_time}) => {
         <h3 className="sub-title">{sub_title}</h3>
         <div className="date_n_time">
           <p className="date">
-            {getDateTime("date")[0]}
-            <sup>{getDateTime("date")[1]}</sup>&nbsp;
-            {getDateTime("date")[2]}
+            {getDateTime("date",date)[0]}
+            <sup>{getDateTime("date",date)[1]}</sup>&nbsp;
+            {getDateTime("date",date)[2]}
           </p>
-          <p className="time">{getDateTime("time")}</p>
-          {/* <p className="time">{useEffect(setInterval(getDateTime("time"),1000),[])}</p> 
-          //https://sebhastian.com/setinterval-react/
-          */}
+          <p className="time">{getDateTime("time",date)}</p>
+          {/* <p className="time">{   
+            useEffect(
+              setTimeout(
+                date.getSeconds(),
+                1000
+              ),[]
+            )
+          }</p> */}
+          {/* <p className="time">{useEffect(setInterval(getDateTime("time",date),2000),[])}</p>  */}
+          {/* //https://sebhastian.com/setinterval-react/ */}
+         
         </div>
       </section>
     </header>
