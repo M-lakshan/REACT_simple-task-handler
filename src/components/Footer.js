e95b08c96ce01e80174f9cb329b0424ebfc126ee
M-lakshan <emlakshan92@gmail.com>
2023-08-15 22:58:28 +0530

    minor changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,14 +3,14 @@ import EditPreview from './EditPreview.js';
 import AddTask from './AddTask.js';
 import {useState} from 'react';
 
-const Footer = () => {
+const Footer = ({taskManage,curEdit}) => {
   const [editPreview,setEditPreveiw] = useState(false);
   const [searchPreview,setSearchPreveiw] = useState(false);
 
   return (
     <footer className="App-footer">
       <section className="task_edit_preview">
-        {(editPreview) && <EditPreview comp_st={[editPreview,setEditPreveiw]}/>}
+        {(editPreview) && <EditPreview comp_st={[editPreview,setEditPreveiw]} edit={curEdit} tskMng={taskManage}/>}
       </section>
       <section className="main_actions">
         <SearchForTasks comp_st={[searchPreview,setSearchPreveiw]}/>
