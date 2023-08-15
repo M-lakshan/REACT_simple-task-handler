d1bb8b146ebc67a7bf6750f45d6230fcac540306
M-lakshan <emlakshan92@gmail.com>
2023-08-15 17:17:39 +0530

    structured

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,11 +1,20 @@
+import SearchForTasks from './SearchForTasks.js';
+import EditPreview from './EditPreview.js';
+import AddTask from './AddTask.js';
+import {useState} from 'react';
+
 const Footer = () => {
+  const [editPreview,setEditPreveiw] = useState(false);
+  const [searchPreview,setSearchPreveiw] = useState(false);
+
   return (
     <footer className="App-footer">
-      <section className="">
-
+      <section className="task_edit_preview">
+        {(editPreview) && <EditPreview comp_st={[editPreview,setEditPreveiw]}/>}
       </section>
-      <section className="">
-
+      <section className="main_actions">
+        <SearchForTasks comp_st={[searchPreview,setSearchPreveiw]}/>
+        <AddTask comp_st={[editPreview,setEditPreveiw]}/>
       </section>
     </footer>
   );
