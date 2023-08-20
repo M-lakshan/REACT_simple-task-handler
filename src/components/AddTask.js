3061895e8fed3f0aff98ed9eb79eba17166d36c3
M-lakshan <emlakshan92@gmail.com>
2023-08-20 19:10:13 +0530

    AddTask logic added

--- src/components/AddTask.js
+++ src/components/AddTask.js
@@ -1,9 +1,23 @@
-import React, {useState} from 'react';
+import React from 'react';
 
-const AddTask = () => {
+const AddTask = ({ comp_st, edit }) => {
+  const handleAddTaskClick = () => {
+    edit[1](false,{
+      id: '',
+      name: '',
+      details: '',
+      scheduled_for: '',
+      tags: '',
+      completed: '',
+      removed: ''
+    });
+    comp_st[1](true);
+    document.querySelector('footer').classList.add('focus_out');
+  }
+
   return (
-    <button >
-      <abbr title="Add-Task"><i class="fa-solid fa-plus"></i></abbr>
+    <button id="add_task" onClick={() => handleAddTaskClick()}>
+      <abbr title="Add-Task"><i className="fa-solid fa-plus"></i></abbr>
     </button>
   );
 }
