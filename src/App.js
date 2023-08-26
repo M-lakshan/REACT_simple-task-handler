d7e1a0127c565013bc2e4a8f876bab53bff39d73
M-lakshan <emlakshan92@gmail.com>
2023-08-26 10:34:17 +0530

    new general state added

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
 import Header from './components/Header.js';
 import Main from './components/Main.js';
 import Footer from './components/Footer.js';
+import { tasks } from './components/task_list.js';
 import { useState } from 'react';
 import './styles/App.css';
 
 function App() {
+  const [myTasks,setMyTasks] = useState(tasks);
   const [currentEdit,setCurrentEdit] = useState([false,{}]);
+  const setMyTasksAlt = (t) => setMyTasks(t);
 
   return (
     <div className="App">
       <Header />
       <Main 
         cur_edit_alt={[currentEdit,(edit) => setCurrentEdit(edit)]}
+        cur_tasks_list={[myTasks,(tsks) => setMyTasksAlt(tsks)]}
       />
       <Footer 
         cur_edit_alt={[currentEdit,(edit) => setCurrentEdit(edit)]}
+        cur_tasks_list={[myTasks,(tsks) => setMyTasksAlt(tsks)]}
       />
     </div>
   );
