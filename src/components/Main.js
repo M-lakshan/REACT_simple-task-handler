a71c9219df61a16cc5fc6924115419fdb93a5132
M-lakshan <emlakshan92@gmail.com>
2023-08-26 10:35:39 +0530

    logic changed

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,66 +1,62 @@
-import React, { useState } from 'react';
-import { tasks } from './task_list.js';
+import React from 'react';
 import { TasksUndone, TasksDone, TasksRemoved, TasksMissed } from './Task.js';
-
-const check_missed_task_sts = (sts) => {
-  if(sts.length<2) {
-    return false;
-  } else {
-    let current_date = new Date();
-    let scheduled_date_i = parseInt(sts.substring(0,sts.indexOf('/')));
-    let scheduled_date_ii = parseInt(sts.substring((sts.indexOf('/')+1),sts.length));
-
-    return ((current_date.getDate() > scheduled_date_i) && ((current_date.getMonth()+1) > scheduled_date_ii));
-  }
-}
+import { check_missed_task_sts } from './date_n_time.js';
   
-const Main = ({cur_edit_alt}) => {
-  const [myTasks,setMyTasks] = useState(tasks);
+const Main = ({ cur_edit_alt, cur_tasks_list }) => {
   let tasks_undone = [];
   let tasks_done = [];
   let tasks_missed = [];
   let tasks_removed = [];
+  let alt_myTasks = cur_tasks_list[0];
+  let alt_setMyTasks = cur_tasks_list[1];
 
   const derivate_tasks = (arr) => {
-    let new_arr = [];
-    
+      
     arr.forEach(tsk => {
-      new_arr.push(tsk);
-      if(tsk.completed!==true && check_missed_task_sts(tsk.scheduled_for[0])) {
-        tasks_missed.push(tsk);
-      } else if(tsk.removed) {
-        tasks_removed.push(tsk);
+
+      if(tsk.completed!==true) {
+        if(!check_missed_task_sts(tsk.scheduled_for[0])) {
+          tasks_missed.push(tsk);
+        } else {
+          if(tsk.removed) {
+            tasks_removed.push(tsk);
+          } else {
+            tasks_undone.push(tsk);
+          }
+        }
       } else if(tsk.completed && tsk.removed!==true) {
         tasks_done.push(tsk);
+      } else if(tsk.removed) {
+        tasks_removed.push(tsk);
       } else {
         tasks_undone.push(tsk);
       }
     });
   }
 
-  derivate_tasks(myTasks);
+  derivate_tasks(alt_myTasks);
 
   return (
     <main className="App-main">
       <section className="tasks">
         <TasksUndone 
           arr={tasks_undone}
-          tsk_st={[myTasks,(update) => setMyTasks(update)]}
+          tsk_st={[alt_myTasks,(update) => alt_setMyTasks(update)]}
           cur_edit={cur_edit_alt}
         />
         <TasksDone 
           arr={tasks_done}
-          tsk_st={[myTasks,(update) => setMyTasks(update)]}
+          tsk_st={[alt_myTasks,(update) => alt_setMyTasks(update)]}
           cur_edit={cur_edit_alt}
         />
         <TasksRemoved 
           arr={tasks_removed}
-          tsk_st={[myTasks,(update) => setMyTasks(update)]}
+          tsk_st={[alt_myTasks,(update) => alt_setMyTasks(update)]}
           cur_edit={cur_edit_alt}
         />
         <TasksMissed 
           arr={tasks_missed}
-          tsk_st={[myTasks,(update) => setMyTasks(update)]}
+          tsk_st={[alt_myTasks,(update) => alt_setMyTasks(update)]}
           cur_edit={cur_edit_alt}
         />
       </section>
