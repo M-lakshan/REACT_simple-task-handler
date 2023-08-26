ee90b8a6f2aa017bf47e2ca49ba27b56c1fb3626
M-lakshan <emlakshan92@gmail.com>
2023-08-26 10:34:51 +0530

    property values changed

--- src/components/task_list.js
+++ src/components/task_list.js
@@ -3,55 +3,55 @@ export const tasks = [
     id: 1,
     name: "do grocery",
     details: "by dairy products & eggs",
-    scheduled_for: ["08/18",""],
+    scheduled_for: ["18/08",""],
     completed: false,
     removed: false,
-    tag: ''
+    tag: false
   },
   {
     id: 2,
     name: "take the trash out",
     details: "",
     scheduled_for: ["",""],
-    completed: true,
-    removed: true,
-    tag: ''
+    completed: false,
+    removed: false,
+    tag: false
   },
   {
     id: 3,
     name: "clean up the fridge",
     details: "",
     scheduled_for: ["",""],
-    completed: false,
+    completed: true,
     removed: true,
-    tag: ''
+    tag: "imp"
   },
   {
     id: 4,
     name: "practice more in programming",
     details: "follow REACT programming trends, common patters for applications",
-    scheduled_for: ["08/20",""],
+    scheduled_for: ["20/12",""],
     completed: false,
     removed: false,
-    tag: ''
+    tag: "imp"
   },
   {
     id: 5,
     name: "attend the Buisiness meeting",
     details: "@8.30pm on regional-offce",
-    scheduled_for: ["08/20","07:00pm"],
+    scheduled_for: ["20/11","07:00pm"],
     completed: false,
     removed: false,
-    tag: ''
+    tag: "urg"
   },
   {
     id: 6,
     name: "be home by 11.00pm",
     details: "...hv to sleep ealry in order to attend head-office tommorrow",
-    scheduled_for: ["08/20","10:00pm"],
+    scheduled_for: ["20/11","10:00pm"],
     completed: false,
     removed: false,
-    tag: ''
+    tag: "imp"
   },
   {
     id: 7,
@@ -59,7 +59,7 @@ export const tasks = [
     details: "gonna rain soon... better to clean the roof",
     scheduled_for: ["",""],
     completed: false,
-    removed: true,
-    tag: ''
+    removed: false,
+    tag: "pvt"
   },
 ];
