7fd4a8ed089e36b9fcdb17c91f06cf518e7574b1
M-lakshan <emlakshan92@gmail.com>
2023-08-26 10:32:15 +0530

    eml_main_height_changer.js removed

--- src/components/elm_main_height_change.js
+++
@@ -1,5 +0,0 @@
-export const elm_main_height_change = () => {
-  let new_height = window.innerHeight - 20 -
-    (document.querySelector('header').clientHeight + document.querySelector('footer').clientHeight);
-  document.querySelector('main').style.height = `${new_height}px`;
-}
